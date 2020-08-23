743e03f1f3d07116fcb5ca36e0675a380a1c663d
Abner Felipe <abnfel@gmail.com>
2020-08-23 14:03:31 -0300

    Sistema de like implementado

--- src/Componentes/Lista/index.js
+++ src/Componentes/Lista/index.js
@@ -7,7 +7,49 @@ export default class index extends Component {
         this.state = {
             feed: this.props.data
         };
+        this.mostraLikes = this.mostraLikes.bind(this);
+        this.like = this.like.bind(this);
+        this.carregaIcone = this.carregaIcone.bind(this);
     }
+
+    mostraLikes(likers) {
+        let feed = this.state.feed;
+        if (feed.likers <= 0) {
+            return;
+        }
+        return (
+            <Text style={styles.likers}>
+                {feed.likers} {feed.likers > 1 ? 'curtidas' : 'curtida'}
+            </Text>
+        );
+    }
+
+    like() {
+        let feed = this.state.feed;
+
+        if (feed.likeada === true) {
+            this.setState({
+                feed: {
+                    ...feed,
+                    likeada: false,
+                    likers: feed.likers - 1
+                }
+            });
+        } else {
+            this.setState({
+                feed: {
+                    ...feed,
+                    likeada: true,
+                    likers: feed.likers + 1
+                }
+            });
+        }
+    }
+
+    carregaIcone(likeada) {
+        return likeada ? require('../../img/likeada.png') : require('../../img/like.png')
+    }
+
     render() {
         return (
             <View style={styles.areaFeed}>
@@ -24,9 +66,9 @@ export default class index extends Component {
                     style={styles.fotoPublicacao}
                 />
                 <View style={styles.areaBtn}>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={this.like}>
                         <Image
-                            source={require('../../img/like.png')}
+                            source={this.carregaIcone(this.state.feed.likeada)}
                             style={styles.like}
                         />
                     </TouchableOpacity>
@@ -37,6 +79,8 @@ export default class index extends Component {
                         />
                     </TouchableOpacity>
                 </View>
+
+                {this.mostraLikes(this.state.feed.likers)}
                 <View style={styles.viewRodape}>
                     <Text style={styles.nomeRodape}>
                         {this.state.feed.nome}
@@ -55,51 +99,55 @@ const styles = StyleSheet.create({
 
     },
     nomeUsuario: {
-        fontSize:22,
-        textAlign:'left',
-        color:'#000'
+        fontSize: 22,
+        textAlign: 'left',
+        color: '#000'
     },
     fotoPerfil: {
-        width:50,
-        height:50,
-        borderRadius:25
+        width: 50,
+        height: 50,
+        borderRadius: 25
     },
     viewPerfil: {
-        flexDirection:'row',
-        flex:1,
-        alignItems:'center',
-        padding:8
-        
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center',
+        padding: 8
+
     },
     fotoPublicacao: {
-        flex:1,
-        height:400,
-        alignItems:'center'
+        flex: 1,
+        height: 400,
+        alignItems: 'center'
     },
-    like:{
-        width:33,
-        height:33
+    like: {
+        width: 33,
+        height: 33
     },
-    areaBtn:{
-        flexDirection:'row',
-        padding:5
+    areaBtn: {
+        flexDirection: 'row',
+        padding: 5
     },
-    btnSend:{
-        paddingLeft:5,
+    btnSend: {
+        paddingLeft: 5,
     },
-    viewRodape:{
-        flexDirection:'row',
-        alignItems:'center'
+    viewRodape: {
+        flexDirection: 'row',
+        alignItems: 'center'
     },
-    descRodape:{
-        paddingLeft:5,
-        fontSize:15,
-        color:'#000',
+    descRodape: {
+        paddingLeft: 5,
+        fontSize: 15,
+        color: '#000',
     },
-    nomeRodape:{
-        fontSize:18,
-        fontWeight:'bold',
-        color:'#000',
-        paddingLeft:5
+    nomeRodape: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#000',
+        paddingLeft: 5
+    },
+    likers: {
+        fontWeight: 'bold',
+        marginLeft: 5
     }
 })
